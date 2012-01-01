28df842b76409917a87ca813af3593b11be86ca9
Alexander Nasonov <alnsn@yandex.ru>
2012-01-01 17:59:19 +0000

    Fix copyright year.

--- benchmark/c.c
+++ benchmark/c.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2011-2012 Alexander Nasonov.
+ * Copyright (c) 2012 Alexander Nasonov.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
