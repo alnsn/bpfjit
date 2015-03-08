9a40326f5cf179021666869fa38af1700dc61db9
Alexander Nasonov <alnsn@yandex.ru>
2015-03-08 23:38:50 +0000

    Fix copyright.

--- src/bpf-compat.h
+++ src/bpf-compat.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ * Copyright (c) Alexander Nasonov, 2015.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
